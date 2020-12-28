3fc0b81a4b0d83988f6cf094fcb4c70eed2aface
ranjan <ranjan@data-sleek.com>
2020-12-27 19:24:56 -0800

    First method added: get_player_match

--- src/UnderstatPlayer.py
+++ src/UnderstatPlayer.py
@@ -1,6 +1,33 @@
 from bs4 import BeautifulSoup
+import requests
+import re
+from constants import *
+import json
 
 class UnderstatPlayer():
 
+    id = -1
+
     def __init__(self, player_id):
         self.id = player_id
+
+    def get_player_match_data(self):
+        r = requests.get(PLAYER_URL.format(self.id))
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        scripts = soup.find_all("script")
+
+        def string_escape(s, encoding='utf-8'):
+            return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
+                     .decode('unicode-escape') # Perform the actual octal-escaping decode
+                     .encode('latin1')         # 1:1 mapping back to bytes
+                     .decode(encoding))        # Decode original encoding
+
+        data_pattern = re.compile(DATA_PATTERN.format(PLAYER_MATCHES_DATA))
+
+        for script in scripts:
+            match = re.search(data_pattern, str(script))
+            if match:
+                break
+
+        return string_escape(match.group(1))
