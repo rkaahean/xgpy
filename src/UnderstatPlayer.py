fd165f853567d57fcb7132cb11558d34afc41c38
ranjan <ranjan@data-sleek.com>
2020-12-27 18:39:36 -0800

    Initializing only via ID

--- src/UnderstatPlayer.py
+++ src/UnderstatPlayer.py
@@ -2,6 +2,5 @@ from bs4 import BeautifulSoup
 
 class UnderstatPlayer():
 
-    def __init__(self):
-        self.name = ''
-        self.id = -1
+    def __init__(self, player_id):
+        self.id = player_id
