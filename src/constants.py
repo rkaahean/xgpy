1172031ee7e538b400827d0a0d1f97cc41d3a6c0
ranjan <ranjan@data-sleek.com>
2020-12-27 19:25:16 -0800

    Added constants for player_match_data

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,8 @@
-PLAYER_URL = 'https://understat.com/player/{}'
+# MAIN URL's
+PLAYER_URL = "https://understat.com/player/{}"
+
+# Pattern of the JSON variables
+DATA_PATTERN = "{}\s+=\s+JSON.parse\(\'(.*)\'\)"
+
+# JSON_CONSTANTS: The variable names which prefix the data
+PLAYER_MATCHES_DATA = "matchesData"
