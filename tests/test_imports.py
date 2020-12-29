4c3ca36adf47b050f0fb83105a10727eb16b3c8f
ranjan <ranjan@data-sleek.com>
2020-12-29 15:42:20 -0800

    Updated tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,7 +3,7 @@
 class TestImport():
 
     def test_imports(self):
-        from src.understat import UnderstatPlayer
-        from src.Utility import Utility
+        from understat import UnderstatPlayer
+        from Utility import Utility
 
         assert 1 == 1
