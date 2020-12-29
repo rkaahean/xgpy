ee74d3e69c60f0ee6aa728a93de39a94b3914d65
ranjan <ranjan@data-sleek.com>
2020-12-28 16:06:34 -0800

    Added a url checker and a request object checker

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,7 +1,15 @@
-# from Utility import Utility
+from Utility import Utility
+from constants import PLAYER_URL, TEST_PLAYER_ID, TEST_PLAYER_URL
 
 class TestUtility:
 
-    def test_hi(self):
-        x = "this"
-        assert "h" in x
+    def test_generate_url_player(self):
+
+        # assert that the url generated is as intended
+        assert Utility.generate_request_url(PLAYER_URL, TEST_PLAYER_ID) == TEST_PLAYER_URL
+
+    def test_generate_request_object(self):
+        url = Utility.generate_request_url(PLAYER_URL, TEST_PLAYER_ID)
+
+        # assert that the request went through well
+        assert Utility.generate_request_object(url).status_code == 200
