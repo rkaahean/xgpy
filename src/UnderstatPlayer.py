be8c87a974222af39f71414f46d767b185faf3bb
ranjan <ranjan@data-sleek.com>
2020-12-28 19:41:16 -0800

    Updated documentation

--- src/UnderstatPlayer.py
+++ src/UnderstatPlayer.py
@@ -3,15 +3,55 @@ from src.constants import *
 
 class UnderstatPlayer():
 
+    """
+    A class that represents an understat football player logically.
+
+    ...
+
+    Attributes
+    ----------
+    id : int
+        the id of the player instance in understat.
+
+    Methods
+    -------
+    get_player_match_data()
+        get xG data for the player in question.
+
+    """
+
     id = -1
 
     def __init__(self, player_id):
+        """
+        Parameter
+        --------
+        id : int
+            the id of the player who's data is needed.
+        """
+
         self.id = player_id
 
     def get_player_match_data(self):
+        """
+        get the match data for the player.
+
+        Returns
+        -------
+        json
+            a json object contatining the information.
+
+        """
+
+        # First generate the URL from which data will be fetched
+        # Next, generate a request object based on the URL
+        # Finally, using regular expressions, finding the json
+        # data using the keyword for the query
 
         base_url = Utility().generate_request_url(PLAYER_URL, self.id)
         r = Utility().generate_request_object(base_url)
         match = Utility().find_match(r, PLAYER_MATCHES_DATA)
+
+        # TODO: need to add filtering params
 
         return Utility.string_escape(match.group(1))
--- src/Utility.py
+++ src/Utility.py
@@ -5,28 +5,103 @@ import json
 
 class Utility():
 
+    """
+    A class comprising of all the helper functions.
+
+    Methods
+    -------
+    generate_request_url(base_url, param)
+        construct the URL to be queried.
+
+    generate_request_object(url)
+        generate a request object for the url specified.
+
+    string_escape(s, encoding='utf-8')
+        convert the string from given encoding (default utf-8) to human readable text.
+
+    find_match(request, match_string)
+        find a match in the request based on the match string provided.
+
+    """
+
     @staticmethod
     def generate_request_url(base_url: str, param: str):
+        """
+        Parameters
+        ----------
+        base_url: str
+            the core understat url for the related endpoint. see constants.py for example.
+        param: str
+            value to format the string with (team or player id).
+
+        Returns
+        -------
+        str
+            a url string to fetch the data from.
+        """
+
         return base_url.format(param)
 
     @staticmethod
     def generate_request_object(url: str):
+        """
+        Parameters
+        ---------
+        url: str
+            the url to which a connection is to be made.
+
+        Raises
+        ------
+        Exception
+            A generic exception when a connection cannot be established.
+
+        """
+
         try:
             return requests.get(url)
         except Exception:
+            # TODO: Add a more detailed explanation
             print("An error occured")
 
     @staticmethod
     def string_escape(s, encoding='utf-8'):
+        """
+        Parameters
+        ----------
+        s: str
+            the string to be decoded.
+        encoding: str
+            the encoding with which the string is to be converted.
+
+        Returns
+        -------
+        str
+            a human readable string
+
+        """
+
         return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
                  .decode('unicode-escape') # Perform the actual octal-escaping decode
                  .encode('latin1')         # 1:1 mapping back to bytes
                  .decode(encoding))        # Decode original encoding
 
     @staticmethod
-    def find_match(request, DATA_CONSTANT:str):
+    def find_match(request, match_string:str):
+        """
+        Parameters
+        ----------
+        request: object
+            a request object that sucessfully connected to the desired URL.
+        match_string: str
+            the string used to find a match. Depends on the type of data being queried for. see constants.
 
-        match_pattern = re.compile(DATA_PATTERN.format(DATA_CONSTANT))
+        Returns
+        -------
+        Match
+            returns a regex match object
+        """
+
+        match_pattern = re.compile(DATA_PATTERN.format(match_string))
         match = re.search(match_pattern, request.text)
 
         return match
