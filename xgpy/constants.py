aac9d0715755ef733f025857e0e37b75de4000d7
ranjan <ranjan@data-sleek.com>
2020-12-31 02:03:06 -0800

    added team testing constants

--- xgpy/constants.py
+++ xgpy/constants.py
@@ -12,13 +12,14 @@ PLAYER_SHOT_DATA = "shotsData"
 PLAYER_MIN_MAX_DATA = "minMaxPlayerStats"
 PLAYER_POSITIONS = "positionsList"
 PLAYER_LIST_DATA = "playersData"
+TEAMS_STANDINGS_DATA = "teamsData"
 
 # Filter constants
 ARG_SEASON = "season"
 ARG_START_DATE = 'start_date'
 ARG_END_DATE = 'end_date'
 ARG_POSITION = 'position'
-PLAYER_DATA_COLUMNS = [
+PLAYER_DATA_COLUMNS = [  # for get_player_list_by_league
         'id',
         'player_name',
         'team_title'
@@ -30,3 +31,6 @@ TEST_FILTER = {
 }
 TEST_PLAYER_ID = 1228
 TEST_PLAYER_URL = "https://understat.com/player/{}".format(TEST_PLAYER_ID)
+TEST_SEASON = 2020
+TEST_LEAGUE = 'EPL'
+TEST_TEAM = 'Manchester_United'
