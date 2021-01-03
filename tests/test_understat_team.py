5addf77709dbbc8f3fa5193907d3f58b535595e3
ranjan <ranjan@data-sleek.com>
2021-01-02 17:37:01 -0800

    added testing for Utility string conversion

--- tests/test_understat_team.py
+++ tests/test_understat_team.py
@@ -4,7 +4,19 @@ from xgpy.understat import UnderstatTeam
 class TestUnderstatTeam():
 
     def test_get_team_league_history(self):
+
+        # test that the data received is of type dictionary and not empty
+
         tp = UnderstatTeam(TEST_TEAM, TEST_LEAGUE)
         data = tp.get_team_league_history(TEST_SEASON)
 
         assert isinstance(data, dict) and len(data) > 0
+
+    def test_get_team_player_summary(self):
+
+        # test that the data received is of type dictionary and not empty
+
+        tp = UnderstatTeam(TEST_TEAM, TEST_LEAGUE)
+        data = tp.get_team_player_summary(TEST_SEASON)
+
+        assert isinstance(data, list) and len(data) > 0
