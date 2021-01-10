fd9e1c5bc91fd43a39f73d4727bae40cc64fc0a6
ranjan <ranjan@data-sleek.com>
2021-01-09 23:58:48 -0800

    added testing for fbrefPlayer class

---
+++ tests/test_fbref_player.py
@@ -0,0 +1,12 @@
+from xgpy.constants_fbref import *
+from xgpy.fbref import fbrefPlayer
+
+class TestfbrefPlayer():
+
+    def test_get_player_aggregate_stats(self):
+
+        tp = fbrefPlayer(TEST_PLAYER_ID)
+        for stat in FBREF_STATS_TO_CLASS_MAP.keys():
+
+            data = tp.get_player_aggregate_stats(stat, 'all_competitions')
+            assert (isinstance(data, dict) and len(data) > 0)
