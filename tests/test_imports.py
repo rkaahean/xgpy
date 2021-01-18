0ca7b8ebba07fece0602e0ecc2cb96b7c5833fcb
ranjan <ranjan@data-sleek.com>
2021-01-18 11:03:42 -0800

    updated imports for testing

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -2,6 +2,7 @@ class TestImport():
 
     def test_imports(self):
         from xgpy.utility import Utility
-        from xgpy.understat import UnderstatPlayer
+        from xgpy.understat import UnderstatPlayer, UnderstatTeam, UnderstatMatch
+        from xgpy.fbref import fbrefPlayer, fbrefTeam
 
         assert 1 == 1
