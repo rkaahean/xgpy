c40ad7ca3b1875bee201468053c642944ee79cbd
ranjan <ranjan@data-sleek.com>
2021-01-21 16:08:29 -0800

    added tests for competition and player names

--- tests/test_fbref_team.py
+++ tests/test_fbref_team.py
@@ -3,7 +3,7 @@ from xgpy.fbref import fbrefTeam
 
 class TestfbrefPlayer():
 
-    def test_get_player_aggregate_stats(self):
+    def test_get_team_aggregate_stats(self):
 
         tp = fbrefTeam(TEST_TEAM_ID)
         for stat in FBREF_TEAM_STATS_TO_URL_MAP.keys():
@@ -11,3 +11,17 @@ class TestfbrefPlayer():
             # might need to add more competitions
             data = tp.get_team_aggregate_stats(TEST_SEASON, 'all_comps', stat)
             assert (isinstance(data, dict) and len(data) > 0)
+
+    def test_get_team_competition_names(self):
+
+        tp = fbrefTeam(TEST_TEAM_ID)
+
+        data = tp.get_team_competition_names(TEST_SEASON)
+        assert (isinstance(data, dict) and len(data) > 0)
+
+    def test_get_team_player_ids(self):
+
+        tp = fbrefTeam(TEST_TEAM_ID)
+
+        data = tp.get_team_player_ids(TEST_SEASON)
+        assert (isinstance(data, dict) and len(data) > 0)
