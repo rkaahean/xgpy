8ac50c267e6c522c6b44843f709fc6329894bd42
ranjan <ranjan@data-sleek.com>
2021-01-21 16:08:10 -0800

    added league table test

--- tests/test_understat_team.py
+++ tests/test_understat_team.py
@@ -38,3 +38,13 @@ class TestUnderstatTeam():
         data = tp.get_team_grouped_date_by_type(TEST_SEASON)
 
         assert isinstance(data, dict) and len(data) > 0
+
+
+    def test_get_team_league_table(self):
+
+        # test that the data received is of type dictionary and not empty
+
+        tp = UnderstatTeam(TEST_TEAM, TEST_LEAGUE)
+        data = tp.get_team_league_table(TEST_SEASON)
+
+        assert isinstance(data, dict) and len(data) > 0
