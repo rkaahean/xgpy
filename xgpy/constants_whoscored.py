e6a9e3a0d075d2d9dc1c979950a18c4e4fdd9f43
ranjan <ranjan@data-sleek.com>
2021-01-24 15:05:08 -0800

    whoscored constants init

---
+++ xgpy/constants_whoscored.py
@@ -0,0 +1,9 @@
+
+# data pattern
+WHOSCORED_DATA_PATTERN = r"{}\s+=\s+(.*)"
+
+# the url's for whoscored
+MATCH_LIVE_URL = "https://www.whoscored.com/Matches/{}/Live/"
+
+# key variables for scraping whoscored
+MATCH_EVENT_DATA = "matchCentreData"
